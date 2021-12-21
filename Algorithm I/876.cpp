824bb3b142087a70e21d7f460d076f255b507401
Felix <nampanya.f@northeastern.edu>
2021-12-20 22:57:20 -0500

    876 added

---
+++ Algorithm I/876.cpp
@@ -0,0 +1,26 @@
+// 876. Middle of the Linked List
+struct ListNode {
+  int val;
+  ListNode* next;
+  ListNode() : val(0), next(nullptr) {}
+  ListNode(int x) : val(x), next(nullptr) {}
+  ListNode(int x, ListNode* next) : val(x), next(next) {}
+};
+ListNode* middleNode(ListNode* head) {
+    ListNode* fast = head;
+    ListNode* slow = head;
+
+    while (fast != nullptr && fast->next != nullptr){
+        slow = slow->next;
+        fast = fast->next->next;
+    }
+
+    return slow;
+
+}
+
+int main(){
+
+
+    return 0;
+}
