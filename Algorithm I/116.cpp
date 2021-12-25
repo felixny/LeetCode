be593f118350ada1567248ed31f72428cbfece4f
Felix <nampanya.f@northeastern.edu>
2021-12-24 21:53:05 -0500

    166 DFS

---
+++ Algorithm I/116.cpp
@@ -0,0 +1,49 @@
+// 116. Populating Next Right Pointers in Each Node
+
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+// Definition for a Node.
+class Node {
+ public:
+  int val;
+  Node* left;
+  Node* right;
+  Node* next;
+
+  Node() : val(0), left(NULL), right(NULL), next(NULL) {}
+
+  Node(int _val) : val(_val), left(NULL), right(NULL), next(NULL) {}
+
+  Node(int _val, Node* _left, Node* _right, Node* _next)
+      : val(_val), left(_left), right(_right), next(_next) {}
+};
+
+class Solution {
+ public:
+  Node* connect(Node* root) {
+    if (root == nullptr) {
+      return root;
+    }
+
+    Node* leftNode = root;
+    while (leftNode->left != nullptr) {
+      Node* head = leftNode;
+      while (head != nullptr) {
+        head->left->next = head->right;
+        if (head->next != nullptr) {
+          head->right->next = head->next->left;
+        }
+        head = head->next;
+      }
+      leftNode = leftNode->left;
+    }
+
+    return root;
+  }
+};
+
+int main() { return 0; }
