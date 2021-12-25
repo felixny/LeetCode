58b0c52a5ec8c94c53100f20a5a5003542bc4a25
Felix <nampanya.f@northeastern.edu>
2021-12-25 16:01:08 -0500

    188 DP

--- Data Structure I/118.cpp
+++ Data Structure I/118.cpp
@@ -7,14 +7,40 @@
 using namespace std;
 
 class Solution {
-public:
-    vector<vector<int>> generate(int numRows) {
-        
+ public:
+  vector<vector<int>> generate(int numRows) {
+    vector<vector<int>> result;
+
+    if (numRows <= 0) return result;
+
+    result.push_back({1});
+
+    for (int i = 1; i < numRows; i++) {
+      vector<int> row;
+      row.push_back(1);
+       //cout << i << endl;
+      for (int j = 1; j < i; j++){
+          //cout << i << endl;
+       //cout << j << endl;  
+        row.push_back(result[i - 1][j - 1] + result[i - 1][j]);
+      }
+
+      row.push_back(1);
+      result.push_back(row);
     }
+    return result;
+  }
 };
 
 int main() {
-  vector<int> nums = {7, 1, 5, 3, 6, 4};
 
-  cout << Solution().maxProfit(nums) << endl;
+    for (int i = 1; i < 5; i++){
+        for (int j = 1 ; j < i; j++){
+            cout << "#";
+        }
+        cout << endl;
+    }
+  Solution().generate(5);
+
+  return 0;
 }
