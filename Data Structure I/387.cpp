e9519c437156ab5c4a2fd315d40951e218f66aa1
Felix <nampanya.f@northeastern.edu>
2021-12-28 10:19:36 -0500

    206 iterative

---
+++ Data Structure I/387.cpp
@@ -0,0 +1,23 @@
+// 387. First Unique Character in a String
+
+#include <algorithm>
+#include <iostream>
+#include <map>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    int firstUniqChar(string s) {
+        
+    }
+};
+
+int main(){
+
+    string s = "leetcode";
+    string s1 = "loveleetcode";
+
+    return 0;
+}
