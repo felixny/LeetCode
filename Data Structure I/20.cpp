420109d50c59765bce48854f84909c0159f0fee4
Felix <nampanya.f@northeastern.edu>
2021-12-29 12:15:35 -0500

    20 stack

---
+++ Data Structure I/20.cpp
@@ -0,0 +1,36 @@
+// 20. Valid Parentheses
+#include <iostream>
+#include <stack>
+#include <string>
+#include <vector>
+using namespace std;
+
+class Solution {
+ public:
+  bool isValid(string s) {
+    stack<char> t;
+    for (int i = 0; i < s.length(); i++) {
+      if (s[i] == '(' || s[i] == '[' || s[i] == '{') {
+        t.push(s[i]);
+      } else if (s[i] == ')' && !t.empty() && t.top() == '(') {
+        t.pop();
+      } else if (s[i] == ']' && !t.empty() && t.top() == '[') {
+        t.pop();
+      } else if (s[i] == '}' && !t.empty() && t.top() == '{') {
+        t.pop();
+      } else {
+        return false;
+      }
+    }
+    return t.empty();
+  }
+};
+
+int main() {
+  string s = "()";
+  string s1 = "()[]{}";
+  string s2 = "(]";  // false
+  cout << Solution().isValid(s);
+  cout << Solution().isValid(s1);
+  cout << Solution().isValid(s2);
+}
