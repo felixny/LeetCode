30a9006866d517b56253b5dcce0dd6fb696be0b1
Felix <nampanya.f@northeastern.edu>
2021-12-29 10:45:05 -0500

    203 two pointers

---
+++ Data Structure I/203.cpp
@@ -0,0 +1,28 @@
+// 203. Remove Linked List Elements
+
+struct ListNode {
+  int val;
+  ListNode* next;
+  ListNode() : val(0), next(nullptr) {}
+  ListNode(int x) : val(x), next(nullptr) {}
+  ListNode(int x, ListNode* next) : val(x), next(next) {}
+};
+
+class Solution {
+ public:
+  ListNode* removeElements(ListNode* head, int val) {
+    while (head != nullptr && head->val == val) {
+      head = head->next;
+    }
+    ListNode* current = head;
+    while (current != nullptr && current->next != nullptr) {
+      if (current->next->val == val) {
+        current->next = current->next->next;
+      } else {
+        current = current->next;
+      }
+    }
+
+    return head;
+  }
+};
