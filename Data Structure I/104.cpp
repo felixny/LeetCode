8f24d98aeef4a4991481dbeb41bb0e9bdb5931da
Felix <nampanya.f@northeastern.edu>
2022-01-02 10:46:28 -0500

    104 binary tree max depth recursive

---
+++ Data Structure I/104.cpp
@@ -0,0 +1,33 @@
+// 104. Maximum Depth of Binary Tree
+
+#include <algorithm>
+#include <iostream>
+#include <map>
+#include <queue>
+#include <vector>
+
+using namespace std;
+
+struct TreeNode {
+  int val;
+  TreeNode *left;
+  TreeNode *right;
+  TreeNode() : val(0), left(nullptr), right(nullptr) {}
+  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+  TreeNode(int x, TreeNode *left, TreeNode *right)
+      : val(x), left(left), right(right) {}
+};
+
+class Solution {
+ public:
+  int maxDepth(TreeNode *root) {
+      if (root == nullptr){
+          return 0;
+      }
+
+      int left = maxDepth(root->left);
+      int right = maxDepth(root->right);
+
+      return max(left, right) + 1;
+  }
+};
