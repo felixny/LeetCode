3168f799814e8155d2d7b26d16e806bb302d2da4
Felix <nampanya.f@northeastern.edu>
2022-01-18 14:26:53 -0500

    15 3Sum

--- Data Structure II/15.cpp
+++ Data Structure II/15.cpp
@@ -49,7 +49,7 @@ class Solution {
         if (nums[i] + nums[j] + nums[k] == 0) {
           ans.push_back({nums[i], nums[j], nums[k]});
           ++j;
-          if (j && nums[j] == nums[j - 1]) ++j;
+          while (j < k && nums[j] == nums[j - 1]) ++j;
         } else if (nums[i] + nums[j] + nums[k] < 0) {
           ++j;
         } else {
