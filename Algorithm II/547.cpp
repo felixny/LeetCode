44ed2644745cecc6b79e227d4479feed7d3f2d0c
Felix <nampanya.f@northeastern.edu>
2022-01-20 18:48:26 -0500

    547 DFS

--- Algorithm II/547.cpp
+++ Algorithm II/547.cpp
@@ -13,13 +13,16 @@ class Solution {
  public:
   void dfs(vector<vector<int>>& isConnected, int i, int j) {
     isConnected[i][j] = 0;
+    // check row
     for (auto k = 0; k < isConnected.size(); k++) {
       if (isConnected[j][k] == 1) dfs(isConnected, j, k);
     }
   }
   int findCircleNum(vector<vector<int>>& isConnected) {
     int maxProvinces = 0;
+    // next row
     for (auto i = 0; i < isConnected.size(); i++) {
+        // next col
       for (auto j = 0; j < isConnected[0].size(); j++) {
         if (isConnected[i][j] == 1) {
           maxProvinces++;
