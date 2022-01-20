aa7fcb65ed19a153b739597407f1ffe9a597afcf
Felix <nampanya.f@northeastern.edu>
2022-01-20 12:50:15 -0500

    209 minSubArrayLen Sliding Window

---
+++ Algorithm II/209.cpp
@@ -0,0 +1,39 @@
+// 209. Minimum Size Subarray Sum
+
+#include <limits.h>
+
+#include <algorithm>
+#include <iostream>
+#include <map>
+#include <set>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+ public:
+  int minSubArrayLen(int target, vector<int>& nums) {
+    int n = nums.size();
+    int sum = 0;
+    int left = 0;
+    int right = 0;
+    int shortest = INT_MAX;
+
+    while (right < n) {
+      sum += nums[right];
+
+      if (sum >= target) {
+        while (sum >= target) {
+          sum -= nums[left];
+          left++;
+        }
+        shortest = min(shortest, right - left + 2);
+      }
+      right++;
+    }
+    return shortest == INT_MAX ? 0 : shortest;
+  }
+};
+
+int main() { return 0; }
