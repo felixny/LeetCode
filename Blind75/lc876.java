9567d4033d4364f6945dc2d15813aeda47125ed5
Felix <felix8x419x@gmail.com>
2022-07-13 19:18:51 -0400

    middle of the linked list

---
+++ Blind75/lc876.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+public class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
+
+public class lc876 {
+
+
+    public ListNode middleNode(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+}
