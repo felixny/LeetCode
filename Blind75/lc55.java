2b18599123ee355d8b6986f15286664911a867cb
Felix <77823268+felixny@users.noreply.github.com>
2022-07-25 18:18:41 -0400

    greedy

---
+++ Blind75/lc55.java
@@ -0,0 +1,13 @@
+public class lc55 {
+    public boolean canJump(int[] nums) {
+        int canJump = 0;
+        int n = nums.length;
+        for (int i = 0; i < n ; i++){
+            // if initial value is 0 then its impossible
+            if(canJump < i) return false;
+            canJump = Math.max(canJump, i + nums[i]);
+        }
+        
+        return true;
+    }
+}
