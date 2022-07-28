a77951111e85614bd170d9dab7ff5b637d9ecb8e
Felix <77823268+felixny@users.noreply.github.com>
2022-07-27 23:32:41 -0400

    min heap

--- Blind75/lc215.java
+++ Blind75/lc215.java
@@ -1,19 +1,27 @@
 import java.util.PriorityQueue;
 
-import java.util.*;
-
 public class lc215 {
     public int findKthLargest(int[] nums, int k){
         PriorityQueue<Integer> heap = new PriorityQueue<>();
-
+        // min heap
         for (int n : nums){
             heap.add(n);
-
+            System.out.println(heap);
             if (heap.size() > k){
-                heap.poll();
+                System.out.println("heap: " + heap);
+                System.out.println("removed: " + heap.poll());
+              
             }
         }
 
         return heap.peek();
+    }
+
+    public static void main(String[] args) {
+        lc215 lc = new lc215();
+        int[] nums = {3,2,1,5,6,4};
+        int k = 2;
+        lc.findKthLargest(nums, k);
+
     }
 }
